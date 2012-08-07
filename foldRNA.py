2e5774aecdeeea23a8df0884436eea97af1ec293
Matt Larson <mateotambien@gmail.com>
2012-08-07 12:52:36 -0700

    limits on where hairpin can form

--- foldRNA.py
+++ foldRNA.py
@@ -2,24 +2,32 @@ import sys
 import subprocess
 import random
 
+## Need to run the python command in the c:\UNAfold folder, otherwise program won't properly write folding energies into the output file ##
+
 def foldRNA(pausefilename,outfilename):
-  pausefile=open(pausefilename, 'rU')
+  pausefile=open(pausefilename, 'rU') ## full version of longseq pause file ##
   outfile=open(outfilename,'wb')
-   
+  outfile.write('pause_fold'+'\t'+'upstream_sequence'+'\t'+'deltaG'+'\n')
+  
   for line in pausefile:
-    pausestring=line.strip('\n')
-    pauselist=list(pausestring)
-    random.shuffle(pauselist)
-    pausestring=''.join(pauselist)
-    currentpausefile=open('c:\UNAFold\currentpauseseq','w')
-    currentpausefile.write(pausestring)
-    currentpausefile.close()
-    mfold=subprocess.call('perl c:\UNAFold\UNAFold.pl c:\UNAFold\currentpauseseq',stderr=subprocess.STDOUT,stdout=subprocess.PIPE,shell=True)
-    deltaGfile=open('c:\UNAFold\currentpauseseq.dG','r')
-    deltaGstring=deltaGfile.readlines()
-    deltaGfile.close()
-    deltaG=deltaGstring[1].split('\t')[1]
-    outfile.write(pausestring+'\t'+deltaG+'\n')
+    if line[0]=='>':
+      pauserow=line.strip('>').strip('\n').split('_')
+      pause_fold=pauserow[8]
+      outfile.write(pause_fold+'\t')
+    elif line[0]!='>':
+      pausestring=line.strip('\n')[0:29]
+      #pauselist=list(pausestring)
+      #random.shuffle(pauselist)
+      #pausestring=''.join(pauselist)
+      currentpausefile=open('c:\UNAFold\currentpauseseq','w')
+      currentpausefile.write(pausestring)
+      currentpausefile.close()
+      mfold=subprocess.call('perl c:\UNAFold\UNAFold.pl --constraints=c:\UNAFold\constraints.txt c:\UNAFold\currentpauseseq',stderr=subprocess.STDOUT,stdout=subprocess.PIPE,shell=True)
+      deltaGfile=open('c:\UNAFold\currentpauseseq.dG','r')
+      deltaGstring=deltaGfile.readlines()
+      deltaGfile.close()
+      deltaG=deltaGstring[1].split('\t')[1]
+      outfile.write(pausestring+'\t'+deltaG+'\n')
     
   pausefile.close()
   outfile.close()
